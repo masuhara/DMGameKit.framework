969e24f706607dd1d7f980ec87738eff11ed1c9f
Daisuke Masuhara <info@masuhara.net>
2015-03-13 14:26:21 +0900

    Initial Commit

---
+++ DMGameKit/DMGameKit.h
@@ -0,0 +1,13 @@
+//
+//  DMGameKit.h
+//  DMGameKit
+//
+//  Created by Master on 2015/03/13.
+//  Copyright (c) 2015年 net.masuhara. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface DMGameKit : NSObject
+
+@end
---
+++ DMGameKit/DMGameKit.m
@@ -0,0 +1,13 @@
+//
+//  DMGameKit.m
+//  DMGameKit
+//
+//  Created by Master on 2015/03/13.
+//  Copyright (c) 2015年 net.masuhara. All rights reserved.
+//
+
+#import "DMGameKit.h"
+
+@implementation DMGameKit
+
+@end
